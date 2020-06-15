e92f85f2acabf7d5e08376d31d40bd98a4234ab4
mehedi rana <mdmehedirana@gmail.com>
2020-06-15 11:48:11 +0600

    add nativebase

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
 
 const SignUpScreen =()=>{
    return(
        <View>
-           <Text> hi</Text>
+           <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
        </View>
    )
 }
