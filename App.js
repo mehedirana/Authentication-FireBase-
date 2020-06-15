4f8164adc07821d5b09878ad5529ff5f958cccf9
mehedi rana <mdmehedirana@gmail.com>
2020-06-15 11:45:18 +0600

    add fkg

--- App.js
+++ App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignUpScreen from './screens/SignUpScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Stack.Navigator>
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-     
-    </Stack.Navigator>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
     </View>
   );
 }
@@ -21,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const SignUpScreen =()=>{
    return(
-       <view>
+       <View>
            <Text> hi</Text>
-       </view>
+       </View>
    )
 }
 
