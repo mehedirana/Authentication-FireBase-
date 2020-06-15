1dff2df55920d28bd0aedef5fa1dd55178dec902
mehedi rana <mdmehedirana@gmail.com>
2020-06-15 11:38:36 +0600

    add stack navigation

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignUpScreen from './screens/SignUpScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Stack.Navigator>
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+     
+    </Stack.Navigator>
     </View>
   );
 }
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const SignUpScreen =()=>{
-
+   return(
+       <view>
+           <Text> hi</Text>
+       </view>
+   )
 }
 
 export default SignUpScreen;
