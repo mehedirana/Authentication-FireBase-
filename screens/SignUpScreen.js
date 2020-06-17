ce7d6cc73dff2cbb21e5a3dff20347c5984cb55d
mehedi rana <mdmehedirana@gmail.com>
2020-06-17 06:58:44 +0600

    add header

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Item, Input, Label, Button } from 'native-base';
+import { Item, Input, Label, Button, Header, Left, Icon, Body, Title, Right } from 'native-base';
 
 const SignUpScreen =()=>{
    return(
-       <View style={{padding: 10}}>
+       <View style={{padding: 0}}>
+         <Header span>
+          <Left>
+          </Left>
+          <Body>
+            <Title>Create Account</Title>
+          </Body>
+          <Right />
+        </Header>
            <Item floatingLabel>
               <Label>Username</Label>
               <Input />
             </Item>
-            <Item floatingLabel last>
+            <Item floatingLabel>
               <Label>Password</Label>
               <Input />
             </Item>
-            <Button rounded>
+            <Button block style={{justifyContent:'center', padding:20}}>
             <Text>Primary</Text>
           </Button> 
 
