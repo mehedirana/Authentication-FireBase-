84cce205d1f41019982797714afcd96e80c08ab4
mehedi rana <mdmehedirana@gmail.com>
2020-06-17 07:31:06 +0600

    add keyboardavoiding view and style

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, KeyboardAvoidingView } from 'react-native';
 import { Item, Input, Label, Button, Header, Left, Icon, Body, Title, Right } from 'native-base';
 
-const SignUpScreen =()=>{
-   return(
-       <View style={{padding: 0}}>
-         <Header span>
-          <Left>
-          </Left>
-          <Body>
-            <Title>Create Account</Title>
-          </Body>
-          <Right />
-        </Header>
-           <Item floatingLabel>
-              <Label>Username</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel>
-              <Label>Password</Label>
-              <Input />
-            </Item>
-            <Button block style={{justifyContent:'center', padding:20}}>
-            <Text>Primary</Text>
-          </Button> 
+const SignUpScreen = () => {
+  return (
+    <KeyboardAvoidingView >
+      <Header span>
+        <Left>
+        </Left>
+        <Body>
+          <Title style={{ fontSize: 18, fontWeight:'bold'}}>Create Account</Title>
+        </Body>
+        <Right />
+      </Header>
 
-       </View>
-   )
+      <View style={{flexDirection:"column", padding:15}}>
+
+        <Item floatingLabel style={{borderBottomColor:'blue', padding:5}}>
+          <Label>Username</Label>
+          <Input />
+        </Item>
+
+        <Item floatingLabel style={{borderBottomColor:'blue', padding:5}} >
+          <Label>Password</Label>
+          <Input />
+        </Item>
+
+        <Button block  style={{ justifyContent: 'center',}}>
+          <Text style={{color:'white', fontWeight:'bold'}}>sign up</Text>
+        </Button>
+
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
 
 export default SignUpScreen;
