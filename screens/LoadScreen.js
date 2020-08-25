4da72c223cea094281750d41b9715178e914548c
mehedi rana <mdmehedirana@gmail.com>
2020-08-25 14:43:26 +0600

    ab

---
+++ screens/LoadScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Text,View,  KeyboardAvoidingView, TouchableOpacity} from 'react-native';
+import { Item, Input, Label, Button, Header, Left,  Body, Title, Right} from 'native-base';
+
+const SignInScreen = ({ navigation }) =>{
+    return (
+        <KeyboardAvoidingView >
+          <Header span>
+            <Left>
+            </Left>
+            <Body>
+              <Title>please log in</Title>
+            </Body>
+            <Right />
+          </Header>
+    
+          <View style={{flexDirection:"column", padding:15}}>
+    
+            <Item floatingLabel style={{borderBottomColor:'blue', padding:5}}>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+    
+            <Item floatingLabel style={{borderBottomColor:'blue', padding:5}} >
+              <Label>Password</Label>
+              <Input />
+            </Item>
+    
+            <Button block  style={{ justifyContent: 'center',}}>
+              <Text style={{color:'white'}}>sign in</Text>
+            </Button>
+    
+            <TouchableOpacity
+            onPress={()=>{navigation.navigate("SignUpScreen")}}
+            >
+              <Text style={{textAlign:'center', padding:10,  fontWeight:'bold'}}>don't have an account?</Text>
+            </TouchableOpacity>
+    
+          </View>
+        </KeyboardAvoidingView>
+      )
+
+}
+export default SignInScreen;
